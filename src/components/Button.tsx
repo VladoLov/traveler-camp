f63adc9832c3f48b9c3916c09d08ad214fcfcc52
VladoLov <vladislav.lovric@gmail.com>
2024-11-26 07:42:46 +0100

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import React from "react";
+
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant:
+    | "btn_dark_green"
+    | "btn_green"
+    | "btn_white_text"
+    | "btn_white"
+    | "btn_dark_green_outline";
+  full?: boolean;
+};
+
+export default function Button({
+  type,
+  title,
+  icon,
+  variant,
+  full,
+}: ButtonProps) {
+  return (
+    <button
+      className={`flex items-center  gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
+      type={type}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
+    </button>
+  );
+}
